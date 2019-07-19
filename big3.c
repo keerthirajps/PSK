e42da4f7c176121858a6ae3a780f6d1e48c629ba
keerthirajps <52620200+keerthirajps@users.noreply.github.com>
2019-07-19 12:50:20 +0530

    Update big3.c

--- big3.c
+++ big3.c
@@ -5,9 +5,9 @@ void big3()
     printf("Enter three numbers: ");
     scanf("%lf %lf %lf", &n1, &n2, &n3);
     if( n1>=n2 && n1>=n3 )
-        printf("%.2f is the largest number./n", n1);
+        printf("%.2f is the largest number..", n1);
     if( n2>=n1 && n2>=n3 )
-        printf("%.2f is the largest number./n", n2);
+        printf("%.2f is the largest number..", n2);
     if( n3>=n1 && n3>=n2 )
-        printf("%.2f is the largest number./n", n3);
+        printf("%.2f is the largest number..", n3);
 }
